8c8540386633e5184f93fdfbeaaf8f0dedfb7396
Dorado <47840353+the-real-dorado@users.noreply.github.com>
2025-08-10 18:15:41 +0530

    fixed utilities

--- CFD/data.py
+++ CFD/data.py
@@ -10,30 +10,30 @@ cmap_alpha=LinearSegmentedColormap.from_list('cmap_alpha',[color_w,color_b],2)
 def plot(fluid,plots=['velocity','streamlines'],*,path='',info=False):
    name='plot'
    for _ in plots: name=name+'-'+_
-   if in_cli(): print(f'Rendering: {name}')
+   if in_cli(): print(f'Plotting: {name}')
    F=fluid
-   figure(figsize=((F.Nx-1)/(F.Ny-1)*10,10),tight_layout=True,facecolor='w')
+   figure(figsize=((F.Nx-1)/(F.Ny-1)*5,5),tight_layout=True,facecolor='w')
    if 'pressure' in plots: 
       contourf(F.X,F.Y,F.p.T,cmap='bwr',levels=20)
-      if info: title('\nPressure [Pa]\n',fontdict={'fontsize':30})
+      if info: title(r'Specific Pressure ($m^2/s^2$)',fontdict={'fontsize':15})
    if 'velocity' in plots: 
-      contourf(F.X,F.Y,(F.vx.T**2+F.vy.T**2)**0.5,cmap='Blues',levels=20)
-      if info: title('\nVelocity [m/s]\n',fontdict={'fontsize':30})
+      contourf(F.X,F.Y,(F.Ux.T**2+F.Uy.T**2)**0.5,cmap='Blues',levels=20)
+      if info: title(r'Velocity ($m/s$)',fontdict={'fontsize':15})
    if info: colorbar()
-   if 'streamlines' in plots: streamplot(F.X[2:-3][2:-3],F.Y[2:-3][2:-3],F.vx.T[2:-3][2:-3],F.vy.T[2:-3][2:-3],color='k',density=2,arrowstyle='->')
+   if 'streamlines' in plots: streamplot(F.X[2:-3][2:-3],F.Y[2:-3][2:-3],F.Ux.T[2:-3][2:-3],F.Uy.T[2:-3][2:-3],color='k',linewidth=1,density=2,arrowstyle='->')
    contourf(F.x,F.y,-F.domain.T,cmap=cmap_alpha)
    axis('scaled')
    axis('off')
-   savefig(f'{path}{name}_it-{F.it}_d-{F.d:.5f}_t-{F.t//60:.0f}m{F.t%60:.0f}s.png',dpi=200,bbox_inches='tight')
+   if path: savefig(f'{path}{name}_it-{F.it}_re-{F.re:.5f}_t-{F.t//60:.0f}m{F.t%60:.0f}s.png',dpi=200,bbox_inches='tight')
    if in_cli(): print('------------------------------------')
 
 def export(fluid,path=''):
    name='export'
    F=fluid
    print(f'Exporting')
-   with open(f'{path}{name}_it-{F.it}_d-{F.d:.5f}_t-{F.t//60:.0f}m{F.t%60:.0f}s.csv','w+') as f:
-      f.write('x,y,vx,vy,p\n')
+   with open(f'{path}{name}_it-{F.it}_re-{F.re:.5f}_t-{F.t//60:.0f}m{F.t%60:.0f}s.csv','w+') as f:
+      f.write('x,y,Ux,Uy,p\n')
       for i in range(F.Nx):
          for j in range(F.Ny):
-            f.write(f'{i*F.dl},{j*F.dl},{F.vx[i,j]},{F.vy[i,j]},{F.p[i,j]}\n')
+            f.write(f'{i*F.dl},{j*F.dl},{F.Ux[i,j]},{F.Uy[i,j]},{F.p[i,j]}\n')
    if in_cli(): print('------------------------------------')
--- CFD/utilities.py
+++ CFD/utilities.py
@@ -1,9 +1,9 @@
 from decimal import Decimal
 
-def importobj(file,dl):    # for importing 2D grid of points
-    with open(file) as f:  # Blender .obj export options:
-        l = f.readlines()  # Forward: Y Forward
-    p=[]                   # Up: Z Up
+def importobj(file,dl):
+    with open(file) as f:
+        l = f.readlines()
+    p=[]
     for _ in l:
         if _[0]=='v' and _[1]==' ':
             p.append(_.split()[1:])
@@ -11,26 +11,26 @@ def importobj(file,dl):    # for importing 2D grid of points
         p[i]=(int(Decimal(v[0])/Decimal(dl)),int(Decimal(v[1])/Decimal(dl)))
     return p
 
-def status(it,d,t,cli,mode):
+def status(it,re,t,cli,mode):
     if mode=='run':
         if cli:
-            print(f'iteration: {it}')
-            print(f'abs delta-KE: {d:.5f}')
-            print(f'elapsed time: {t//60:.0f}m {t%60:.0f}s   ')
+            print(f'iteration:       {it}')
+            print(f'U residual:      {re:.5f}')
+            print(f'elapsed time:    {t//60:.0f}m {t%60:.0f}s   ')
             print('\033[1A\033[1A\033[1A',end='\x1b[2K')
         else:
-            print((f'iteration: {it} | abs delta-KE: {d:.5f} | elapsed time: {t//60:.0f}m {t%60:.0f}s   '),end='\r')     
+            print((f'iteration: {it} | U residual: {re:.5f} | elapsed time: {t//60:.0f}m {t%60:.0f}s   '),end='\r')     
     elif mode=='start':
         if cli: 
             print('\033[?25l',end='')
     elif mode=='end':
         if cli: 
             print('\033[?25h',end='')
-            print(f'iteration: {it}')
-            print(f'abs delta-KE: {d:.5f}')
-            print(f'elapsed time: {t//60:.0f}m {t%60:.0f}s   ')
+            print(f'iteration:       {it}')
+            print(f'U residual:      {re:.5f}')
+            print(f'elapsed time:    {t//60:.0f}m {t%60:.0f}s   ')
         else:
-            print((f'iteration: {it} | abs delta-KE: {d:.5f} | elapsed time: {t//60:.0f}m {t%60:.0f}s   '))
+            print((f'iteration: {it} | U residual: {re:.5f} | elapsed time: {t//60:.0f}m {t%60:.0f}s   '))
 
 def in_cli():
     try:
